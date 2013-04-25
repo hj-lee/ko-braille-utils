62e6a08a9b1cf41a4cb4e5a0d3cb80edd251f0dc
HJ Lee <hj.d.lee@gmail.com>
2013-04-25 21:51:15 +0900

    Line number out.

--- ko2br_beauty.py
+++ ko2br_beauty.py
@@ -4,12 +4,15 @@ import sys
 
 from ko_braille import *
 
+cnt = 0
+
 while True:
+    cnt += 1
     line = sys.stdin.readline()
     if not line:
         break
-    sys.stdout.write(line)
+    sys.stdout.write("%3d %s" % (cnt, line))
     line = to_braille(line)
     line = '|'.join(line)
-    line = line.replace(' ', '  ')
+    line = line.replace(' ', '_')
     sys.stdout.write(line)
